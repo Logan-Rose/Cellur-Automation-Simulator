bac936cdbacd8abc7818f5da852e758d18ce6423
Logan-Rose <logan.rose0329@gmail.com>
2020-05-16 00:12:35 -0400

    added functionality to have multiple rulsets, changed speed from slider to 3 diescrete options, added 'roses algorithm' which operates on a different rule set than conway

--- canvas.js
+++ canvas.js
@@ -3,26 +3,101 @@ let context = canvas.getContext("2d")
 canvas.height = 800
 canvas.width = 1600
 
-let cellSize = 40
+let cellSize = 20
 let rows = canvas.height / cellSize
 let columns = canvas.width / cellSize
 console.log(rows)
 console.log(columns)
+let minSpeed = 1000
 let speed = 450
+let mouseDown = false
+let playing = false
 
-let board = [] 
+
+//Initialize a 2 dimensional array for every cell on the board
+let board = []
 for (let i = 0; i < rows; i++) {
     board[i] = new Array(columns);
 }
+clearBoard()
 
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < rows; j++) {
-        board[i][j] = false
+//a function that detects if the whole board is off
+function isOff(){
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < rows; j++) {
+            if(board[i][j]){
+                return false
+            }
+        }
     }
+    return true
 }
 
-let mouseDown = false
-let playing = false
+
+
+//A function that sets the whole board to off
+function clearBoard(){
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < rows; j++) {
+            board[i][j] = false
+        }
+    }
+    console.log(isOff())
+}
+
+//A function to create a gosper glider gun structure
+function gosperGun(){
+    board[5][2] = true
+    board[5][1] = true
+    board[6][1] = true
+    board[6][2] = true
+    board[5][11] = true
+    board[6][11] = true
+    board[7][11] = true
+    board[4][12] = true
+    board[8][12] = true
+    board[3][13] = true
+    board[3][14] = true
+    board[9][13] = true
+    board[9][14] = true
+    board[6][15] = true
+    board[4][16] = true
+    board[8][16] = true
+    board[5][17] = true
+    board[6][17] = true
+    board[7][17] = true
+    board[6][18] = true
+
+    board[3][21] = true
+    board[4][21] = true
+    board[5][21] = true
+    board[3][22] = true
+    board[4][22] = true
+    board[5][22] = true
+
+    board[2][23] = true
+    board[6][23] = true
+
+    board[1][25] = true
+    board[2][25] = true
+    board[6][25] = true
+    board[7][25] = true
+
+    board[3][35] = true
+    board[4][35] = true
+    board[3][36] = true
+    board[4][36] = true
+
+
+
+
+
+
+
+    
+}
+
+
 
 function condition(board, j, i){
     neighbours = []
@@ -76,31 +151,51 @@ function condition(board, j, i){
         neighbours.push(board[j-1][i+1])
         neighbours.push(board[j-1][i-1])
     }
+    return neighbours
+}
+
+function conway(cell, neighbours){
     neighbours.filter(isTrue)
     let neighbourCount = neighbours.filter(isTrue).length
-    if(board[j][i] && (neighbourCount ==2 || neighbourCount == 3)){
+    if(cell && (neighbourCount ==2 || neighbourCount == 3)){
         //console.log("Alive " + neighbourCount +  true)
         return true
-    } else if(!board[j][i] && neighbourCount ==3){
+    } else if(!cell && neighbourCount ==3){
         //console.log("Dead " + neighbourCount + true)
         return true
     }else{
         return false
     }
-
 }
+
+function rose(cell, neighbours){
+    neighbours.filter(isTrue)
+    let neighbourCount = neighbours.filter(isTrue).length
+    if(cell && (neighbourCount % 2 ==0)){
+        //console.log("Alive " + neighbourCount +  true)
+        return true
+    } else if(!cell && (neighbourCount % 2 == 1)){
+        return true
+    }else{
+        return false
+    }
+}
+
+
+
+algorithms = [conway, rose]
 function isTrue(x){
     return x
 }
+
 let gameLoop
-function start(){
-    if(!playing){
-        gameLoop = setInterval(simulate, speed)
-        playing = true
-    }
+function start(speed){
+    clearInterval(gameLoop)
+    gameLoop = setInterval(simulate, speed)
+
 }
+
 function pause(){
-    playing = false
     clearInterval(gameLoop)
 }
 
@@ -110,7 +205,8 @@ function simulate(){
     for (let i = 0; i < rows; i++) {
         row = []
         for (let j = 0; j < columns; j++) {
-            if(condition(board, i, j)){
+            let neighbours = condition(board,i,j)
+            if(algorithms[0](board[i][j], neighbours)){
                 row[j] = true
             } else{
                 row[j] = false
@@ -122,37 +218,14 @@ function simulate(){
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
             if(board[j][i]){
-                fillCell(i,j )
+                fillCell(i,j)
             }
         }
     }
 
 }
 
-
-let slider = document.getElementById('slider')
-
-slider.addEventListener('mousedown', 
-    function(){
-        mouseDown = true
-        document.getElementById("speed").innerHTML = slider.value
-        speed = slider.value
-    }
-)
-slider.addEventListener('mouseup', 
-    function(){
-        mouseDown = false
-    }
-)
-slider.addEventListener('mousemove', 
-    function(){
-        if(mouseDown === true){
-            document.getElementById("speed").innerHTML = slider.value
-            speed = slider.value
-        }
-    }
-)
-
+//Game board listeners
 window.addEventListener('mousedown',
     function(event){
         var rect = canvas.getBoundingClientRect();
@@ -195,20 +268,26 @@ window.addEventListener('mousemove',
         }
     } 
 )
+
+
+//Drawing functions
 function fillCell(xPos, yPos){
     context.beginPath();
     context.fillRect(cellSize * xPos, cellSize * yPos, cellSize, cellSize);
     context.stroke();
 }
 
-function drawBoard(){  
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            if(board[i][j]){
-                fillCell(j, i)
+function drawBoard(){
+    if(!isOff()){
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                if(board[i][j]){
+                    fillCell(j, i)
+                }
             }
         }
     }
+
     for(let i =0; i < canvas.height; i = i + cellSize){
         context.beginPath();
         context.moveTo(0, i);

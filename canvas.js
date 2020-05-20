29cc0970c78e85402837625a52972e539c658532
Logan-Rose <logan.rose0329@gmail.com>
2020-05-19 23:59:24 -0400

    Added Styling

--- canvas.js
+++ canvas.js
@@ -1,9 +1,19 @@
-let canvas = document.querySelector("canvas")
+let canvas = document.getElementById("board")
+let page = document.getElementsByTagName("BODY")[0]
 let context = canvas.getContext("2d")
-canvas.height = 800
-canvas.width = 1600
+let cellSize = 22
 
-let cellSize = 20
+var computed = getComputedStyle(canvas);
+var w = parseInt(computed.getPropertyValue("width"), 10);
+var h = parseInt(computed.getPropertyValue("height"), 10);
+console.log(w  + "," + h )
+window.onresize = function(){ location.reload(); }
+
+
+canvas.height = h - (h % cellSize)
+canvas.width = w - (w % cellSize)
+
+
 let rows = canvas.height / cellSize
 let columns = canvas.width / cellSize
 console.log(rows)
@@ -11,7 +21,6 @@ console.log(columns)
 let minSpeed = 1000
 let speed = 450
 let mouseDown = false
-let playing = false
 
 
 //Initialize a 2 dimensional array for every cell on the board
@@ -24,7 +33,7 @@ clearBoard()
 //a function that detects if the whole board is off
 function isOff(){
     for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < rows; j++) {
+        for (let j = 0; j < columns; j++) {
             if(board[i][j]){
                 return false
             }
@@ -38,11 +47,10 @@ function isOff(){
 //A function that sets the whole board to off
 function clearBoard(){
     for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < rows; j++) {
+        for (let j = 0; j < columns; j++) {
             board[i][j] = false
         }
     }
-    console.log(isOff())
 }
 
 //A function to create a gosper glider gun structure
@@ -67,34 +75,23 @@ function gosperGun(){
     board[6][17] = true
     board[7][17] = true
     board[6][18] = true
-
     board[3][21] = true
     board[4][21] = true
     board[5][21] = true
     board[3][22] = true
     board[4][22] = true
     board[5][22] = true
-
     board[2][23] = true
     board[6][23] = true
-
     board[1][25] = true
     board[2][25] = true
     board[6][25] = true
     board[7][25] = true
-
     board[3][35] = true
     board[4][35] = true
     board[3][36] = true
     board[4][36] = true
-
-
-
-
-
-
-
-    
+   
 }
 
 
@@ -188,13 +185,43 @@ function isTrue(x){
     return x
 }
 
+let playing = false
 let gameLoop
-function start(speed){
-    clearInterval(gameLoop)
-    gameLoop = setInterval(simulate, speed)
-
+let gameSpeed = -1
+function setSpeed(speed){
+    if(speed ==30){
+        document.getElementById("fast").style.background = "#50BFE6"
+        document.getElementById("medium").style.background = "white"
+        document.getElementById("slow").style.background = "white"
+    }else if(speed == 250){
+        document.getElementById("fast").style.background = "white"
+        document.getElementById("medium").style.background = "#50BFE6"
+        document.getElementById("slow").style.background = "white"
+    } else if(speed == 500){
+        document.getElementById("fast").style.background = "white"
+        document.getElementById("medium").style.background = "white"
+        document.getElementById("slow").style.background = "#50BFE6"
+    }
+    if(playing){
+        clearInterval(gameLoop)
+        gameSpeed = speed
+        play()
+    } else{
+        gameSpeed = speed
+    }
+    
 }
 
+function play(){
+    if(gameSpeed == -1){
+        alert("You must pick a speed before playing")
+    }else{
+        playing = true
+        clearInterval(gameLoop)
+        gameLoop = setInterval(simulate, gameSpeed)
+    }
+
+}
 function pause(){
     clearInterval(gameLoop)
 }
@@ -229,10 +256,13 @@ function simulate(){
 window.addEventListener('mousedown',
     function(event){
         var rect = canvas.getBoundingClientRect();
-        if(event.clientX < canvas.width && event.clientY < canvas.height){
+        let mouseX = event.clientX - rect.left
+        let mouseY = event.clientY - rect.top
+        
+        if(mouseX < canvas.width && mouseY < canvas.height){
             mouseDown = true
-            let yPos = Math.floor(event.clientY / cellSize)
-            let xPos = (Math.floor(event.clientX / cellSize))
+            let yPos = Math.floor(mouseY / cellSize)
+            let xPos = Math.floor(mouseX / cellSize)
             board[yPos][xPos] = true
         }
     } 
@@ -240,14 +270,16 @@ window.addEventListener('mousedown',
 
 window.addEventListener('dblclick',
     function(event){
-        console.log("fuck")
         var rect = canvas.getBoundingClientRect();
-        if(event.clientX < canvas.width && event.clientY < canvas.height){
-            let yPos = Math.floor(event.clientY / cellSize)
-            let xPos = (Math.floor(event.clientX / cellSize))
+        let mouseX = event.clientX - rect.left
+        let mouseY = event.clientY - rect.top
+        
+        if(mouseX < canvas.width && mouseY < canvas.height){
+            mouseDown = true
+            let yPos = Math.floor(mouseY / cellSize)
+            let xPos = Math.floor(mouseX / cellSize)
             board[yPos][xPos] = false
         }
-        console.log(board)
     } 
 )
 window.addEventListener('mouseup',
@@ -260,9 +292,14 @@ window.addEventListener('mouseup',
 window.addEventListener('mousemove',
     function(event){
         if(mouseDown){
-            if(event.clientX < canvas.width && event.clientY < canvas.height){
-                let yPos = Math.floor(event.clientY / cellSize)
-                let xPos = (Math.floor(event.clientX / cellSize))
+            var rect = canvas.getBoundingClientRect();
+            let mouseX = event.clientX - rect.left
+            let mouseY = event.clientY - rect.top
+            
+            if(mouseX < canvas.width && mouseY < canvas.height){
+                mouseDown = true
+                let yPos = Math.floor(mouseY / cellSize)
+                let xPos = Math.floor(mouseX / cellSize)
                 board[yPos][xPos] = true
             }
         }
@@ -292,19 +329,19 @@ function drawBoard(){
         context.beginPath();
         context.moveTo(0, i);
         context.lineTo(canvas.width, i);
-        context.strokeStyle = "black";
+        context.strokeStyle = "plum";
         context.lineWidth = 2;
         context.stroke();
-        context.fillStyle = "red";
+        context.fillStyle = "#50BFE6";
     }
     for(let i =0; i < canvas.width; i = i + cellSize){
         context.beginPath();
         context.moveTo(i, 0);
         context.lineTo(i, canvas.height);
-        context.strokeStyle = "black";
+        context.strokeStyle = "plum";
         context.lineWidth = 2;
         context.stroke();
-        context.fillStyle = "red";
+        context.fillStyle = "#50BFE6";
     }
 }
 
